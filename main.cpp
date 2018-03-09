78769926bddc54b91010d07c96f695af31069841
Jason <jpbarne@clemson.edu>
2018-03-09 14:24:54 -0500

    prints input files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
-int main(void) {
-	std::cout << "Hello World" << std::endl;
+void printFile(const std::ifstream&);
+
+int main(int argc, char const *argv[]) {
+	std::ifstream ram, addr, uprog;
+	ram.open("ram.txt");
+	addr.open("addr.txt");
+	uprog.open("uprog.txt");
+
+	//print ram
+	std::cout << "Contents of RAM memory" << std::endl
+	          << "addr value" << std::endl;
+	int i = 0, value;
+	while (ram >> value) {
+		std::cout << i << ": " << std::setfill('0') << std::setw(3) << value << std::endl;
+		i++;
+	}
+
+	//print addr
+	std::cout << "Contents of address ROM" << std::endl
+	          << "opc value" << std::endl;
+	i = 0;
+	while (addr >> std::hex >> value) {
+		std::cout << i << ": " << std::setfill('0') << std::setw(2) << std::hex << value << std::endl;
+		i++;
+	}
+
+	ram.close();
+	addr.close();
+	uprog.close();
 	return 0;
 }
