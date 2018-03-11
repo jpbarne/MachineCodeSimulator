9f86dc22bd425f503499fb6471eee467b67eb958
Jason <jpbarne@clemson.edu>
2018-03-11 15:35:20 -0400

    refactoring

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ std::map <int, std::string> ops
 	 {6, "LD"}, {7, "ED"}, {8, "LI"}, {9, "EI"}, {10, "LA"},
 	 {11, "EA"}, {12, "A"}, {13, "S"}, {14, "EU"}, {15, "LB"},
 	 {16, "CD"}, {17, "MAP"}, {18, "HLT"}};
+	 
 int pc, mar, ir, mdr, acc, b, alu;
 
 std::string instructs[8] = {
@@ -19,68 +20,15 @@ std::string instructs[8] = {
 int RAM[256];
 int wordCnt;
 int ROM[32];
- 
+
+void load_ram();
+void load_rom();
+void load_uprog();
+
 int main(int argc, char const *argv[]) {
-
-	std::ifstream ram, addr, uprog;
-	ram.open("ram.txt");
-	addr.open("addr.txt");
-	uprog.open("uprog.txt");
-
-	//print ram
-	std::cout << "Contents of RAM memory" << std::endl
-	          << "addr value" << std::endl;
-	int i = 0, value;
-	while (ram >> value) {
-		RAM[i] = value & 0xfff;
-		std::cout << i++ << ": " << std::setfill('0') << std::setw(3)
-		          << value << std::endl;
-		if(i >= 256)
-		{
-			std::cout << "RAM overflow" << std::endl;
-			exit(1);
-		}
-	}
-	wordCnt = i;
-	for(; i < 256; i++)
-		RAM[i] = 0;
-
-	//print addr
-	std::cout << std::endl << "Contents of address ROM" << std::endl
-	          << "opc addr" << std::endl;
-	i = 0;
-	while (addr >> std::hex >> value) {
-		std::cout << i++ << ": " << std::setfill('0') << std::setw(2)
-		          << std::hex << value << std::endl;
-	}
-
-	//print control ROM
-	std::cout << "Contents of control ROM with active signals identified" << std::endl
-	          << "addr contents" << std::endl;
-	i = 0;
-	while (uprog >> std::hex >> value) {
-		ROM[i] = value & 0xffffff;
-		//Print out addr contents
-		std::cout << std::setfill(' ') << std::setw(2) << i++ << ": "
-		<< std::setfill('0') << std::setw(6) << std::hex << value << " ";
-
-		for(int code = 0; code < 19; code++)
-		{
-			int mask = (std::pow(2, 23 - code));
-			int result = mask & value;
-			if(result)
-				std::cout << ops[code] << " ";
-			else {
-				if (code == 4 || code == 5 || code == 12 || code == 13) std::cout << "  ";
-				else if (code == 17 || code == 18) std::cout << "    ";
-				std::cout << "   ";
-			}
-		}
-
-		int crja = value & 31;
-		std::cout << " crja=" << std::setfill('0') << std::setw(2) << std::hex << crja
-							<< std::endl;
-	}
+	load_ram();
+	load_rom();
+	load_uprog();
 
 	//print initial register values
 	pc = 0; mar = 0; mdr = 0; acc = 0; alu = 0; b = 0; ir = 0;
@@ -99,10 +47,76 @@ int main(int argc, char const *argv[]) {
 				 << "uc pppm  ddiiaa  ub dpt ja pc mar mdr acc alu   b  ir"
 				 << std::endl;
 
-	
+
+	return 0;
+}
+
+void load_ram() {
+	std::ifstream ram("ram.txt");
+
+	//print ram
+	std::cout << "Contents of RAM memory" << std::endl
+	          << "addr value" << std::endl;
+	int i = 0, value;
+	while (ram >> value) {
+		RAM[i] = value & 0xfff;
+		std::cout << i++ << ": " << std::setfill('0') << std::setw(3)
+		          << value << std::endl;
+		if(i >= 256) {
+			std::cout << "RAM overflow" << std::endl;
+			exit(1);
+		}
+	}
+	wordCnt = i;
+	for(; i < 256; i++)	RAM[i] = 0;
 
 	ram.close();
+}
+
+void load_rom() {
+	std::ifstream addr("addr.txt");
+
+	//print addr
+	std::cout << std::endl << "Contents of address ROM" << std::endl
+	          << "opc addr" << std::endl;
+	int i = 0, value;
+	while (addr >> std::hex >> value) {
+		ROM[i] = value & 0xffffff;
+		std::cout << i++ << ": " << std::setfill('0') << std::setw(2)
+		          << std::hex << value << std::endl;
+	}
+
 	addr.close();
+}
+
+void load_uprog() {
+	std::ifstream uprog("uprog.txt");
+
+	//print control ROM
+	std::cout << "Contents of control ROM with active signals identified" << std::endl
+	          << "addr contents" << std::endl;
+	int i = 0, value;
+	while (uprog >> std::hex >> value) {
+		//Print out uprog contents
+		std::cout << std::setfill(' ') << std::setw(2) << i++ << ": "
+		<< std::setfill('0') << std::setw(6) << std::hex << value << " ";
+
+		for(int code = 0; code < 19; code++)
+		{
+			int mask = (std::pow(2, 23 - code));
+			int result = mask & value;
+			if(result)
+				std::cout << ops[code] << " ";
+			else {
+				if (code == 4 || code == 5 || code == 12 || code == 13) std::cout << "  ";
+				else if (code == 17 || code == 18) std::cout << "    ";
+				std::cout << "   ";
+			}
+		}
+
+		int crja = value & 31;
+		std::cout << " crja=" << std::setfill('0') << std::setw(2) << std::hex
+							<< crja	<< std::endl;
+	}
 	uprog.close();
-	return 0;
 }
